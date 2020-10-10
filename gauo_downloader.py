b2698d859768c972fbfee7bea32bd6cd7af3a787
Dmitrij Rusakov <daemon2017@gmail.com>
2020-10-10 19:20:28 +0300

    Улучшены комментарии

--- gauo_downloader.py
+++ gauo_downloader.py
@@ -15,37 +15,44 @@ page_e = 1000
 url = 'http://www.ogugauo.ru/funds/image.php?f={}&o={}&page={}'
 images_dir = './images/'
 
-if not os.path.exists(images_dir):
-    os.makedirs(images_dir)
 
-error_count = 0
-for f in range(f_s, f_e + 1):
-    print('f:={}'.format(f))
-    for o in range(o_s, o_e + 1):
-        print('o:={}'.format(o))
-        for page in range(page_s, page_e + 1):
-            print('page:={}'.format(page))
-            if not os.path.exists('{}{}_{}_{}.jpg'.format(images_dir, f, o, page)):
-                try:
-                    response = requests.request('GET', url.format(f, o, page))
-                    if len(response.content) > 10:
-                        print('Файл существует!')
-                        filename = wget.download(url.format(f, o, page),
-                                                 out='{}{}_{}_{}.jpg'.format(images_dir, f, o, page))
-                        sleep(0.5)
-                    else:
-                        print('Файл не существует!')
-                        break
-                except ConnectionError as CE:
-                    print(CE)
-                    error_count = error_count + 1
-                except requests.exceptions.ConnectionError as RECE:
-                    print(RECE)
-                    error_count = error_count + 1
-                except requests.exceptions.ChunkedEncodingError as CEE:
-                    print(CEE)
-                    error_count = error_count + 1
-                except URLError as UE:
-                    print(UE)
-                    error_count = error_count + 1
-print('В ходе работы возникло {} ошибок.'.format(error_count))
+def main():
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    error_count = 0
+    for f in range(f_s, f_e + 1):
+        print('f:={}'.format(f))
+        for o in range(o_s, o_e + 1):
+            print('o:={}'.format(o))
+            for page in range(page_s, page_e + 1):
+                print('\npage:={}'.format(page))
+                if not os.path.exists('{}{}_{}_{}.jpg'.format(images_dir, f, o, page)):
+                    try:
+                        response = requests.request('GET', url.format(f, o, page))
+                        if len(response.content) > 10:
+                            print('Файл существует, загружаю!')
+                            wget.download(url.format(f, o, page),
+                                          out='{}{}_{}_{}.jpg'.format(images_dir, f, o, page))
+                            sleep(0.5)
+                        else:
+                            print('Файл не существует, нечего загружать!')
+                            break
+                    except ConnectionError as CE:
+                        print(CE)
+                        error_count = error_count + 1
+                    except requests.exceptions.ConnectionError as RECE:
+                        print(RECE)
+                        error_count = error_count + 1
+                    except requests.exceptions.ChunkedEncodingError as CEE:
+                        print(CEE)
+                        error_count = error_count + 1
+                    except URLError as UE:
+                        print(UE)
+                        error_count = error_count + 1
+    print('В ходе работы возникло {} ошибок.'.format(error_count))
+    input("Нажмите Enter для закрытия окна.")
+
+
+if __name__ == "__main__":
+    main()
